dee25d77bb9f678d662906cb39dea52f9cf4edf6
081fl <ifl.bd2@gmail.com>
2020-05-05 05:41:23 +0700

    main page

---
+++ lib/ui/pages/movie_page.dart
@@ -0,0 +1,84 @@
+part of 'pages.dart';
+
+class MoviePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: accentColor1,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20)),
+          ),
+          padding: EdgeInsets.fromLTRB(defaultMargin, 20, defaultMargin, 30),
+          child: BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) {
+              return state is UserLoaded
+                  ? Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Color(0xff5F558B)),
+                          ),
+                          child: Stack(
+                            children: <Widget>[
+                              SpinKitCircle(
+                                color: accentColor2,
+                                size: 50,
+                              ),
+                              Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: state.user.profilePicture == ""
+                                          ? AssetImage("assets/user_pic.png")
+                                          : NetworkImage(
+                                              state.user.profilePicture),
+                                      fit: BoxFit.cover),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width - 2 * 78,
+                              child: Text(
+                                state.user.name,
+                                style: whiteTextFont.copyWith(fontSize: 18),
+                                maxLines: 1,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                            Text(
+                              NumberFormat.currency(
+                                      locale: "id_ID", decimalDigits: 0, symbol: "IDR ")
+                                  .format(state.user.balance),
+                              style: yellowNumberFont.copyWith(
+                                  fontSize: 14, fontWeight: regularFontWeight),
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  : SpinKitCircle(
+                      color: accentColor2,
+                    );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -6,6 +6,9 @@ import 'package:flutix/services/services.dart';
 import 'package:flutix/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,3 +16,4 @@ part 'main_page.dart';
 part 'sign_in_page.dart';
 part 'wrapper.dart';
 part 'splash_page.dart';
+part 'movie_page.dart';
